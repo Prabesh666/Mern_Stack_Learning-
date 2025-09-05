2ad584c6829e318ca2640d532565c26fe47ab2d2
Prabesh Shah <prabeshshah777@gmail.com>
2025-09-05 15:55:39 +0545

    jdj

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,27 +2,21 @@ import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
 function createJWT(data) {
-    const token = jwt.sign(data, config.jwtSecret, {
-        expiresIn: "1d",
-    });
+  const token = jwt.sign(data, config.jwtSecret, {
+    expiresIn: "1d",
+  });
 
-    return token;
-
+  return token;
 }
 
 async function verifyJWT(authToken) {
+  return await new Promise((resolve, reject) => {
+    jwt.verify(authToken, config.jwtSecret, (error, data) => {
+      if (error) return reject(error);
 
-    return await new Promise((resolve, reject) => {
-        jwt.verify(authToken, config.jwtSecret, (error, data) => {
-            if (error) return reject(error);
-
-            return resolve(data);
-
-        });
-
+      return resolve(data);
     });
+  });
 }
-
-
 
 export { createJWT, verifyJWT };
